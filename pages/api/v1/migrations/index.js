9c934f7fed3138f7abd40a91085fc4dcd5dafb95
Alan Mendes Vieira <alan.mendes@aluno.ufca.edu.br>
2025-12-21 19:17:51 +0000

    Fix `/migrations` endpoint connection bug.

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,43 +3,52 @@ import { join } from "node:path"; // Dados sistema rodando a aplicação, permit
 import db from "infra/database";
 
 export default async function migrations(request, response) {
-  const dbClient = await db.getNewClient();
+  const allowedMethods = ["GET", "POST"];
+  if (!allowedMethods.includes(request.method))
+    return response.status(405).json({
+      error: `Method ${request.method} not allowed`,
+    });
 
-  const defaultMigrationsOptions = {
-    dbClient: dbClient,
-    dryRun: true,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pg-migrations",
-  };
+  let dbClient;
+  try {
+    dbClient = await db.getNewClient();
 
-  if (request.method === "GET") {
-    const migrations = {
-      pendingMigrations: await migrationRunner({
-        ...defaultMigrationsOptions,
-      }),
+    const defaultMigrationsOptions = {
+      dbClient: dbClient,
+      dryRun: true,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pg-migrations",
     };
-    await dbClient.end();
-    return response.status(200).json(migrations);
-  }
 
-  if (request.method === "POST") {
-    const migrations = {
-      appliedMigrations:
-        (await migrationRunner({
+    if (request.method === "GET") {
+      const migrations = {
+        pendingMigrations: await migrationRunner({
           ...defaultMigrationsOptions,
-          dryRun: false,
-        })) ?? [],
-    };
-
-    await dbClient.end();
-
-    if (migrations.appliedMigrations.length > 0) {
-      return response.status(201).json(migrations);
+        }),
+      };
+      return response.status(200).json(migrations);
     }
-    return response.status(200).json(migrations);
-  }
 
-  return response.status(405).end();
+    if (request.method === "POST") {
+      const migrations = {
+        appliedMigrations:
+          (await migrationRunner({
+            ...defaultMigrationsOptions,
+            dryRun: false,
+          })) ?? [],
+      };
+
+      if (migrations.appliedMigrations.length > 0) {
+        return response.status(201).json(migrations);
+      }
+      return response.status(200).json(migrations);
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
+    await dbClient.end();
+  }
 }
