813ee1d954ef8f8415846c84bd5743027960655b
Alan Mendes Vieira <alan.mendes@aluno.ufca.edu.br>
2025-12-26 16:57:16 +0000

    adds `wait-for-postgres.js` script.

---
+++ infra/scripts/wait-for-postgres.js
@@ -0,0 +1,24 @@
+const { exec } = require("node:child_process");
+const { Spinner } = require("cli-spinner");
+
+const spinner = new Spinner("🔴 Aguardando o Postgres aceitar conexões... %s");
+spinner.setSpinnerString("⠄⠆⠇⠋⠙⠸⠰⠠⠰⠸⠙⠋⠇⠆");
+spinner.start();
+
+function checkPostgres() {
+  exec(
+    "docker exec postgres-dev pg_isready --host localhost",
+    (error, stdout) => {
+      if (!stdout || !stdout.includes("accepting connections")) {
+        // Recurção assíncrona(chama a função novamente após um segundo).
+        setTimeout(checkPostgres, 1000); // espera 1s
+        return;
+      }
+
+      spinner.stop(false);
+      console.log("\n🟢 Postgres está pronto e aceitando conexões.");
+    },
+  );
+}
+
+checkPostgres();
