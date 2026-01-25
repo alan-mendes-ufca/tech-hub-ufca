a3b16e28def9a0ab747f89ae96048907839a984b
Alan Mendes Vieira <alan.mendes@aluno.ufca.edu.br>
2026-01-25 19:21:33 -0300

    feat: add `MethodNotAlloewdError` and `ServiceError`

--- infra/errors.js
+++ infra/errors.js
@@ -6,7 +6,47 @@ export class InternalServerError extends Error {
 
     this.name = "InternalServerError";
     this.action = "Entre em contato com o suporte.";
-    this.statusCode = 500;
+    this.statusCode = cause.statusCode;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class ServicesError extends Error {
+  constructor({ message, cause }) {
+    super(message || "Ocorreu um erro de serviço.", {
+      cause,
+    });
+
+    this.name = "ServicesError";
+    this.action = "Verifique a disponibilidade do serviço";
+    this.statusCode = 503;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class MethodNotAllowedError extends Error {
+  constructor() {
+    super("Método não permitido para este endpoint.");
+
+    this.name = "MethodNotAllowedError";
+    this.action = "Verifique se o método HTTP enviado para esse endpoint.";
+    this.statusCode = 405;
   }
 
   toJSON() {
