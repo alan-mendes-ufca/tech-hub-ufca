cb5b1da5534be1fb80305f9b18b61fe72b239908
Alan Mendes Vieira <alan.mendes@aluno.ufca.edu.br>
2026-01-31 15:45:23 -0300

    refactor: make `api/v1/migrations` use `migrator` model

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,10 +1,6 @@
 import { createRouter } from "next-connect";
-import migrationRunner from "node-pg-migrate";
-import { resolve } from "node:path";
-import db from "infra/database";
 import controller from "infra/controller";
-
-const MIGRATIONS_DIR = resolve("infra", "migrations");
+import migrator from "models/migrator";
 
 const router = createRouter();
 
@@ -13,44 +9,11 @@ router.get(getHandler).post(postHandler);
 export default router.handler(controller.errorHandlers);
 
 async function getHandler(request, response) {
-  const isDryRun = true;
-  createMigrationHandler(request, response, isDryRun);
+  response.status(200).json(await migrator.listPendingMigrations());
 }
 
 async function postHandler(request, response) {
-  const isDryRun = false;
-  createMigrationHandler(request, response, isDryRun);
-}
-
-async function createMigrationHandler(request, response, isDryRun) {
-  let dbClient;
-
-  try {
-    dbClient = await db.getNewClient();
-
-    const result = await migrationRunner({
-      dbClient: dbClient,
-      dryRun: isDryRun,
-      dir: MIGRATIONS_DIR,
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pg-migrations",
-    });
-
-    if (isDryRun) {
-      return response.status(200).json({
-        pendingMigrations: result ?? [],
-      });
-    } else if (!isDryRun && result.length > 0) {
-      return response.status(201).json({
-        appliedMigrations: result,
-      });
-    } else {
-      return response.status(200).json({
-        appliedMigrations: [],
-      });
-    }
-  } finally {
-    await dbClient.end();
-  }
+  const result = await migrator.runPedingMigrations();
+  console.log("RESULT: ", result);
+  response.status(result.appliedMigrations.length > 0 ? 201 : 200).json(result);
 }
