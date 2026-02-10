7b0a9c99c19a979fd2e79b5fd6aeed113922c647
Alan Mendes Vieira <alan.mendes@aluno.ufca.edu.br>
2026-02-10 20:43:20 -0300

    feat: use pepper in password hasing

--- models/password.js
+++ models/password.js
@@ -2,7 +2,13 @@ import bcryptjs from "bcryptjs";
 
 async function hash(password) {
   const rounds = getNumberOfRounds();
-  return await bcryptjs.hash(password, rounds);
+
+  if (!process.env.PASSWORD_PEPPER) {
+    throw new Error("PASSWORD_PEPPER não está configurado!");
+  }
+
+  const passwordWithPepper = password + process.env.PASSWORD_PEPPER;
+  return await bcryptjs.hash(passwordWithPepper, rounds);
 
   function getNumberOfRounds() {
     return process.env.NODE_ENV === "production" ? 14 : 1;
@@ -10,7 +16,12 @@ async function hash(password) {
 }
 
 async function compare(providedPassword, storedPassword) {
-  return await bcryptjs.compare(providedPassword, storedPassword);
+  if (!process.env.PASSWORD_PEPPER) {
+    throw new Error("PASSWORD_PEPPER não está configurado!");
+  }
+
+  const passwordWithPepper = providedPassword + process.env.PASSWORD_PEPPER;
+  return await bcryptjs.compare(passwordWithPepper, storedPassword);
 }
 
 const password = {
